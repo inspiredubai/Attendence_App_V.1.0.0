ba3fb23476ef1bd3bb6e7c0ea7b578daf90306bd
Afsar riaz <malikafsar594@gmail.com>
2025-03-10 13:02:41 +0500

    mm

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'attendance_app',
+  webDir: 'www'
+};
+
+export default config;
