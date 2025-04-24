a6706e5aec41debc0b9b2b4b30cb33aef803eaa0
ba <ba@bas-MacBook-Pro.local>
2025-04-24 20:37:22 +0400

    mm

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.attendance.app',
   appName: 'attendance_app',
   webDir: 'www',
   server:{
